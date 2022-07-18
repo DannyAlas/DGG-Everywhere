fa0614616d1735f5af1dbe22e20425d6d9f6aee4
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2022-07-17 19:34:40 -0600

    Deleted extension | total rework

--- ext_chrome/src/bg/background.js
+++
@@ -1,6 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ["js/jquery/jquery.min.js", "src/bg/content.js"]
-    });
-  });
