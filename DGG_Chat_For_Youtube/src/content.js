51ab110f3c528ecf3e718276a47f0b56a91c105d
J4Z2Y <danielalas65@gmail.com>
2022-07-18 22:26:32 -0600

    now stores preferance across instances

--- DGG_Chat_For_Youtube/src/content.js
+++ DGG_Chat_For_Youtube/src/content.js
@@ -1,50 +1,31 @@
-var tabUrl = chrome.tabs.query(
-    {active:true},
-    tabs=>{const tab=tabs[0];})
+function isDGGLoaded() {
+    let src = $('#chatframe').attr('src')
+    return src == "https://www.destiny.gg/embed/chat"
+}
 
-const checkbox = document.querySelector("input[name=checkbox]");
-checkbox.addEventListener("change", async(e) => {
-    if (e.target.checked) {
-        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: async() => {
-                chrome.storage.local.set({ "src": $('#chatframe').attr('src'), function() {console.log("src is set to" + $('#chatframe').attr('src'))}});
-                dggChat = $("ytd-live-chat-frame")
-                dggChat.empty()
-                dggChat.css({
-                    "flex-direction": "row",
-                    "-webkit-flex-direction": "row",
-                });
-                url = `https://www.destiny.gg/embed/chat`
-                dggChat.prepend(`<iframe class="dggChat" style="flex: auto;" src="${url}"></iframe>`)
-            }
-        });
-  } else {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-//    let Aurl = [tab.url][0];
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function:  async() => {
+function loadDGG() {
+    chrome.storage.sync.get("checkbox", function(result) {
+        if (result.checkbox == true && isDGGLoaded() == false) {
+            console.log("DGG WILL BE LOADED");
             dggChat = $("ytd-live-chat-frame")
             dggChat.empty()
             dggChat.css({
-                "flex-direction": "column",
-                "-webkit-flex-direction": "column",
+                "flex-direction": "row",
+                "-webkit-flex-direction": "row",
             });
-            chrome.storage.local.get(["src"], function(result) {
-                id = result.src
-                dggChat.prepend(`<!--css-build:shady--><iframe frameborder="0" scrolling="no" id="chatframe" class="style-scope ytd-live-chat-frame" src="${id}"></iframe>
-                <dom-if class="style-scope ytd-live-chat-frame"><template is="dom-if"></template></dom-if>
-                <div id="show-hide-button" class="style-scope ytd-live-chat-frame"><ytd-toggle-button-renderer class="style-scope ytd-live-chat-frame" use-keyboard-focused="" system-icons="" is-paper-button="" button-renderer="true"><a class="yt-simple-endpoint style-scope ytd-toggle-button-renderer" tabindex="-1"><tp-yt-paper-button id="button" class="style-scope ytd-toggle-button-renderer" style-target="host" role="button" tabindex="0" animated="" elevation="0" aria-disabled="false"><!--css-build:shady--><yt-formatted-string id="text" class="style-scope ytd-toggle-button-renderer">Hide chat replay</yt-formatted-string></tp-yt-paper-button></a></ytd-toggle-button-renderer></div>`)
-              });
-        }});
-    }
-});
+            url = "https://www.destiny.gg/embed/chat"
+            dggChat.prepend(`<iframe id="chatframe" class="dggChat" style="flex: auto;" src="${url}"></iframe>`)
+        } else { console.log("DGG WON'T BE LOADED " + result.checkbox + isDGGLoaded()); }
+    });
+};
 
-$('body').on('click', 'a[target="popout"]', function(x) {
-    x.preventDefault();
-    chrome.tabs.create({ url: $(this).prop('href'), active: false });
-    return false;
-});
+function doLoad1() { chrome.storage.sync.get("checkbox", function(result) { console.log("aaaaaaaaaaaaaaaaa " + result.checkbox) }) }
+
+// I FOR THE LIFE OF ME CANNOT GET THIS TO WORK WITHOUT A DELAY! 
+// Checking the DOM fully loads doesn't work.
+
+document.onreadystatechange = function() {
+    if (document.readyState === 'complete') {
+        setTimeout(() => { loadDGG() }, 500);;
+    }
+}
---
+++ DGG_Chat_For_Youtube/src/popup.js
@@ -0,0 +1,57 @@
+const checkbox = document.querySelector("input[name=checkbox]");
+chrome.storage.sync.get(["checkbox"], async function(result) {
+    if (result.checkbox != null) {
+        document.getElementById("checkbox").checked = result.checkbox
+    } else { document.getElementById("checkbox").checked = false }
+});
+
+checkbox.addEventListener("change", async(e) => {
+    if (e.target.checked) {
+        chrome.storage.sync.set({ "checkbox": true });
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: async() => {
+                chrome.storage.sync.set({
+                    "src": $('#chatframe').attr('src'),
+                    function() { console.log("src is set to" + $('#chatframe').attr('src')) }
+                });
+                dggChat = $("ytd-live-chat-frame")
+                dggChat.empty()
+                dggChat.css({
+                    "flex-direction": "row",
+                    "-webkit-flex-direction": "row",
+                });
+                url = "https://www.destiny.gg/embed/chat"
+                dggChat.prepend(`<iframe id="chatframe" class="dggChat" style="flex: auto;" src="${url}"></iframe>`)
+            }
+        });
+    } else {
+        chrome.storage.sync.set({ "checkbox": false });
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: async() => {
+                dggChat = $("ytd-live-chat-frame")
+                dggChat.empty()
+                dggChat.css({
+                    "flex-direction": "column",
+                    "-webkit-flex-direction": "column",
+                });
+                chrome.storage.sync.get(["src"], function(result) {
+                    id = result.src
+                    dggChat.prepend(`<!--css-build:shady--><iframe frameborder="0" scrolling="no" id="chatframe" class="style-scope ytd-live-chat-frame" src="${id}"></iframe>
+                <dom-if class="style-scope ytd-live-chat-frame"><template is="dom-if"></template></dom-if>
+                <div id="show-hide-button" class="style-scope ytd-live-chat-frame"><ytd-toggle-button-renderer class="style-scope ytd-live-chat-frame" use-keyboard-focused="" system-icons="" is-paper-button="" button-renderer="true"><a class="yt-simple-endpoint style-scope ytd-toggle-button-renderer" tabindex="-1"><tp-yt-paper-button id="button" class="style-scope ytd-toggle-button-renderer" style-target="host" role="button" tabindex="0" animated="" elevation="0" aria-disabled="false"><!--css-build:shady--><yt-formatted-string id="text" class="style-scope ytd-toggle-button-renderer">Hide chat replay</yt-formatted-string></tp-yt-paper-button></a></ytd-toggle-button-renderer></div>`)
+                });
+            }
+        });
+    }
+});
+
+$('body').on('click', 'a[target="popout"]', function(x) {
+    x.preventDefault();
+    chrome.tabs.create({ url: $(this).prop('href'), active: false });
+    return false;
+});
