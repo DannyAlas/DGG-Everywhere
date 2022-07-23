b3b8ee7f55156433f063a3b7cfd9cf22fe83711f
J4Z2Y <danielalas65@gmail.com>
2022-07-23 00:14:03 -0600

    remove debugging

--- DGG_Chat_For_Youtube/src/background.js
+++ DGG_Chat_For_Youtube/src/background.js
@@ -2,7 +2,6 @@ chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     currentUrl = tabs[0].url;
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
-            console.log(sender.tab.url)
             sendResponse({currentUrl: sender.tab.url});
         }
       );
