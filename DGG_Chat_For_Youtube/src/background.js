e357a685b7be1df949e79aabc244b5b92513cf68
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2023-01-17 22:24:09 -0700

    loading fixed for twitch and youtube!
    state saving and loading fixed on both youtube and twitch. NOTE: Need to implement a system to detect if the the current tabs chat has dgg and update the button accordingly

--- DGG_Chat_For_Youtube/src/background.js
+++ DGG_Chat_For_Youtube/src/background.js
@@ -1,8 +1,23 @@
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    currentUrl = tabs[0].url;
-    chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-            sendResponse({currentUrl: sender.tab.url});
-        }
-      );
-});
+// for future use of multiple chat sources in the future
+
+// async function activeTab(activeInfo) {
+//     chrome.tabs.query({'active':true,'currentWindow':true}, async function(array_of_tabs){
+//         if (array_of_tabs[0].url.indexOf('youtube.com/watch') > -1) {
+//             // message the content script to update the src
+//             await chrome.tabs.sendMessage(array_of_tabs[0].id, {message: "updateSrc"}, async function(response) {
+//                 let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//                 if(chrome.runtime.lastError || !response) {
+//                     console.log('script is not injected yet')
+//                     chrome.scripting.executeScript({
+//                         target: {tabId: tab.id, allFrames: true},
+//                         files: ['src/content.js'],
+//                     });
+//                 } else if (response){
+//                     console.log(response.message);
+//                 }
+//             });
+//         }
+//     });
+// }
+
+// chrome.tabs.onActivated.addListener(activeTab)
