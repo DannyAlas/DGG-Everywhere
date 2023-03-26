b022a4ba8b184214304760632c419d68fe0bf730
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2023-03-25 21:13:18 -0600

    sync FF with comit 9c03ab9b610fcc061d8fa024cb91fdacabb09b22

--- DGG_Everywhere_FF/src/popup.js
+++ DGG_Everywhere_FF/src/popup.js
@@ -1,202 +1,252 @@
-const checkbox = document.querySelector("input[name=checkbox]");
-chrome.storage.sync.get(["checkbox"], async function(result) {
-    if (result.checkbox != null) {
-        document.getElementById("checkbox").checked = result.checkbox
-    } else { document.getElementById("checkbox").checked = false }
-});
+const checkbox = document.querySelector("input[name=checkbox]")
+chrome.storage.sync.get(["checkbox"], async function (result) {
+	if (result.checkbox != null) {
+		document.getElementById("checkbox").checked = result.checkbox
+	} else {
+		document.getElementById("checkbox").checked = false
+	}
+})
+checkbox.addEventListener("change", async (e) => {
+	if (e.target.checked) {
+		chrome.storage.sync.set({ checkbox: true })
+		let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
-function loadDGGTW() {
-    twitchChat = $(`div[class*=channel-root__right-column]`);
-    
-    console.log(
-        '%c[DGG] %cPEEPO POOFING TWITCH CHAT AWAY',
-        'color: #538CC6',
-        'color: #6F859A',
-    );
-    twitchChat.empty();
-    // console.log("POOF")
-    twitchChat.prepend(`<div style="display:block; height: 100% !important; width: 100% !important;"><iframe id="chatframe" class="dggChat" style="height: 100% !important; width: 100% !important; display: block;" src="https://www.destiny.gg/embed/chat"></iframe></div>`)
-}
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			function: async () => {
+				var DGGurl = "https://www.destiny.gg/embed/chat"
+				if (window.location.href.indexOf("youtube.com/watch") > -1) {
+					// save youtube url to storage for the popup
+					if (
+						document.getElementById("chatframe").getAttribute("src") !== null
+					) {
+						chrome.storage.sync.set({
+							youtubeChatSrc: document
+								.getElementById("chatframe")
+								.getAttribute("src"),
+						})
+					} else {
+						// save youtube url to storage for the popup
+						chrome.storage.sync.set({
+							youtubeChatSrc:
+								"https://www.youtube.com/live_chat?v=" +
+								window.location.href.substring(
+									window.location.href.lastIndexOf("=") + 1
+								),
+						})
+					}
+					// load the dgg chat
+					var currentChat = document.getElementsByTagName(
+						"ytd-live-chat-frame"
+					)[0]
+					console.log(
+						"%c[DGG] %cPEEPO POOFING YOUTUBE CHAT AWAY",
+						"color: #538CC6",
+						"color: #6F859A"
+					)
+					while (currentChat.firstChild) {
+						currentChat.removeChild(currentChat.firstChild)
+					}
+					currentChat.setAttribute(
+						"style",
+						"flex-direction: row -webkit-flex-direction: row"
+					)
+					var newChat = document.createElement("iframe")
+					newChat.setAttribute("id", "dggChat")
+					newChat.setAttribute("class", "chatframe")
+					newChat.setAttribute("style", "flex: auto")
+					newChat.setAttribute("src", DGGurl)
+					currentChat.appendChild(newChat)
+				}
+				if (window.location.href.indexOf("twitch.tv") > -1) {
+					// set the twitch chat src from the window url
+					chrome.storage.sync.set({
+						twitchChatSrc: `https://www.twitch.tv/popout/${window.location.href.substring(
+							window.location.href.lastIndexOf("/") + 1
+						)}/chat?popout=`,
+					})
+					let currentChat = document.getElementsByClassName(
+						"channel-root__right-column"
+					)[0]
+					// get the chatframe and empty it
+					if (currentChat) {
+						console.log(
+							"%c[DGG] %cPEEPO POOFING TWITCH CHAT AWAY",
+							"color: #538CC6",
+							"color: #6F859A"
+						)
+						while (currentChat.firstChild) {
+							currentChat.removeChild(currentChat.firstChild)
+						}
+						let newChat = document.createElement("iframe")
+						newChat.setAttribute("id", "dggChat")
+						newChat.setAttribute("class", "chatframe")
+						newChat.setAttribute(
+							"style",
+							"height: 100% !important; width: 100% !important; display: block;"
+						)
+						newChat.setAttribute("src", DGGurl)
+						currentChat.prepend(newChat)
+					}
+				}
+				if (window.location.href.indexOf("kick.com") > -1) {
+					// save kicks popup chat url to storage
+					chrome.storage.sync.set({
+						kickChatSrc: `https://kick.com/${window.location.href.substring(
+							window.location.href.lastIndexOf("/") + 1
+						)}/chatroom`,
+					})
+					let currentChat = document.getElementsByClassName("chatroom")[0]
+					console.log(
+						"%c[DGG] %cPEEPO POOFING KICK CHAT AWAY",
+						"color: #538CC6",
+						"color: #6F859A"
+					)
+					// remove all children from the chat
+					while (currentChat.firstChild) {
+						currentChat.removeChild(currentChat.firstChild)
+					}
+					// set the flex direction to row
+					currentChat.setAttribute(
+						"style",
+						"flex-direction: row -webkit-flex-direction: row"
+					)
+					// add the iframe with dgg chat
+					let newChat = document.createElement("iframe")
+					newChat.setAttribute("id", "dggChat")
+					newChat.setAttribute("class", "chatframe")
+					newChat.setAttribute("style", "flex: auto")
+					newChat.setAttribute("src", "https://www.destiny.gg/embed/chat")
+					currentChat.appendChild(newChat)
+				}
+				if (window.location.href.indexOf("rumble.com") > -1) {
+					chrome.storage.sync.set({
+						rumbleChatSrc: `https://rumble.com/chat/popup/${document
+							.getElementsByClassName("rumbles-vote")[0]
+							.getAttribute("data-id")}`,
+					})
+					let currentChat =
+						document.getElementsByClassName("chat--container")[0]
+					console.log(
+						"%c[DGG] %cPEEPO POOFING RUMBLE CHAT AWAY",
+						"color: #538CC6",
+						"color: #6F859A"
+					)
+					// remove all children from the chat
+					while (currentChat.firstChild) {
+						currentChat.removeChild(currentChat.firstChild)
+					}
+					// add the iframe with dgg chat
+					let newChat = document.createElement("iframe")
+					newChat.setAttribute("id", "dggChat")
+					newChat.setAttribute("class", "chat--container container")
+					newChat.setAttribute(
+						"style",
+						"height: 660px; width: 400px; border: 0;"
+					)
+					newChat.setAttribute("src", "https://www.destiny.gg/embed/chat")
+					currentChat.appendChild(newChat)
+				}
+			},
+		})
+	} else if (!e.target.checked) {
+		chrome.storage.sync.set({ checkbox: false })
 
-function loadDGGYT() {
-    dggChat = $(`div[class*=channel-root__right-column]`)
-    console.log(
-        '%c[DGG] %cPEEPO POOFING YOUTUBE CHAT AWAY',
-        'color: #538CC6',
-        'color: #6F859A',
-    );
-    dggChat.empty()
-    dggChat.css({
-        "flex-direction": "row",
-        "-webkit-flex-direction": "row",
-    });
-    dggChat.prepend(`<iframe id="chatframe" class="dggChat" style="flex: auto;" src="https://www.destiny.gg/embed/chat"></iframe>`)
-}
+		chrome.tabs.query(
+			{
+				active: true,
+				currentWindow: true,
+			},
+			function (tabs) {
+				const tab = tabs[0]
 
-checkbox.addEventListener("change", async(e) => {
-    if (e.target.checked) {
-        chrome.storage.sync.set({ "checkbox": true });
-        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: async() => {
-                if (window.location.href.indexOf('youtube.com') > -1) {
-                    // set the youtube chat src of the src of the chatframe
-                    chrome.storage.sync.set({"youtubeChatSrc": "https://www.youtube.com/live_chat?v=" + window.location.href.substring(window.location.href.lastIndexOf('=') + 1)})
-                    
-                    // load the dgg chat
-                    loadDGGYT()
-                    
-                } 
-                else if (window.location.href.indexOf('twitch.tv') > -1) {
-
-                    // set the twitch chat src from the window url
-                    chrome.storage.sync.set({"twitchChatSrc": `https://www.twitch.tv/popout/${
-                        window.location.href.substring(
-                            window.location.href.lastIndexOf('/') + 1
-                            )
-                        }/chat?popout=`})
-
-                    // get the chatframe and empty it
-                    if ($(`div[class*=channel-root__right-column]`)) {
-                        loadDGGTW();
-                        if ($(`iframe[class*=dggChat]`).length){
-                            clearInterval(intervalId);
-                        }
-                    }
-                }
-                else if (window.location.href.indexOf('kick.com') > -1) {
-                    // KICK IS NOT A FAN OF JQUERY SO WE HAVE TO USE VANILLA JS
-                    chrome.storage.sync.set({"kickChatSrc": `https://kick.com/${
-                        window.location.href.substring(
-                            window.location.href.lastIndexOf('/') + 1
-                            )
-                        }/chatroom`})
-                    kickChat = document.getElementsByClassName("chatroom")[0]
-                    console.log(
-                        '%c[DGG] %cPEEPO POOFING KICK CHAT AWAY',
-                        'color: #538CC6',
-                        'color: #6F859A',
-                    );
-                    // remove all children from the chat
-                    while (kickChat.firstChild) {
-                        kickChat.removeChild(kickChat.firstChild);
-                    }
-                    // set the flex direction to row
-                    kickChat.setAttribute("style", "flex-direction: row; -webkit-flex-direction: row;")
-                    // add the iframe with dgg chat
-                    chat = document.createElement("iframe")
-                    chat.setAttribute("id", "dggChat")
-                    chat.setAttribute("class", "chatroom")
-                    chat.setAttribute("style", "flex: auto;")
-                    chat.setAttribute("src", "https://www.destiny.gg/embed/chat")
-                    kickChat.appendChild(chat)                        
-                }
-                else if (window.location.href.indexOf('rumble.com') > -1) {
-                    channel_id = document.getElementsByClassName("rumbles-vote")[0].getAttribute("data-id")
-                    chrome.storage.sync.set({"rumbleChatSrc": `https://rumble.com/chat/popup/${
-                        channel_id
-                    }`})
-                    rumbleChat = document.getElementsByClassName("chat--container")[0]
-                    console.log(
-                        '%c[DGG] %cPEEPO POOFING RUMBLE CHAT AWAY',
-                        'color: #538CC6',
-                        'color: #6F859A',
-                    );
-                    // remove all children from the chat
-                    while (rumbleChat.firstChild) {
-                        rumbleChat.removeChild(rumbleChat.firstChild);
-                    }
-                    // add the iframe with dgg chat
-                    chat = document.createElement("iframe")
-                    chat.setAttribute("id", "dggChat")
-                    chat.setAttribute("class", "chat--container container")
-                    chat.setAttribute("style", "width: 400px; border: 0;")
-                    chat.setAttribute("src", "https://www.destiny.gg/embed/chat")
-                    rumbleChat.appendChild(chat)                        
-                }
-            }
-        });
-    } else if (!e.target.checked) {
-        chrome.storage.sync.set({ "checkbox": false });
-
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, function(tabs) {
-            const tab = tabs[0];
-
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: async() => {
-
-                    if (window.location.href.indexOf('youtube.com') > -1) {
-
-                        chrome.storage.sync.get(["youtubeChatSrc"], function(result) {
-                            id = `${result.youtubeChatSrc}`
-                            ytChat = $("ytd-live-chat-frame")
-                            ytChat.empty()
-                            ytChat.css({
-                                "flex-direction": "column",
-                                "-webkit-flex-direction": "column",
-                            });
-                            ytChat.prepend(
-                                `<!--css-build:shady--><iframe frameborder="0" scrolling="no" id="chatframe" class="style-scope ytd-live-chat-frame" src="${id}"></iframe>
-                                <dom-if class="style-scope ytd-live-chat-frame"><template is="dom-if"></template></dom-if>
-                                <div id="show-hide-button" class="style-scope ytd-live-chat-frame"><ytd-toggle-button-renderer class="style-scope ytd-live-chat-frame" use-keyboard-focused="" system-icons="" is-paper-button="" button-renderer="true"><a class="yt-simple-endpoint style-scope ytd-toggle-button-renderer" tabindex="-1"><tp-yt-paper-button id="button" class="style-scope ytd-toggle-button-renderer" style-target="host" role="button" tabindex="0" animated="" elevation="0" aria-disabled="false"><!--css-build:shady--><yt-formatted-string id="text" class="style-scope ytd-toggle-button-renderer">Hide chat replay</yt-formatted-string></tp-yt-paper-button></a></ytd-toggle-button-renderer></div>`)
-                                });
-                    }
-
-                    else if (window.location.href.indexOf('twitch.tv') > -1) {
-                        chrome.storage.sync.get("twitchChatSrc", function(src) {
-                            src = src.twitchChatSrc
-                            twitchChat = $(`div[class*=channel-root__right-column]`);
-                            twitchChat.empty();
-                            twitchChat.prepend(`<div style="display:block; height: 100% !important; width: 100% !important;"><iframe style="display:block; height: 100% !important; width: 100% !important;" src="${src}"></iframe></div>`);
-                        });
-                    }
-
-                    else if (window.location.href.indexOf('kick.com') > -1) {
-                        chrome.storage.sync.get("kickChatSrc", function(src) {
-                            src = src.kickChatSrc
-                            kickChat = document.getElementsByClassName("chatroom")[0]
-                            while (kickChat.firstChild) {
-                                kickChat.removeChild(kickChat.firstChild);
-                            }
-                            kickChat.setAttribute("style", "flex-direction: column; -webkit-flex-direction: column;")
-                            chat = document.createElement("iframe")
-                            chat.setAttribute("id", "dggChat")
-                            chat.setAttribute("class", "chatroom")
-                            chat.setAttribute("style", "flex: auto;")
-                            chat.setAttribute("src", src)     
-                            kickChat.appendChild(chat)
-                        });
-                    }
-                    else if (window.location.href.indexOf('rumble.com') > -1) {
-                        chrome.storage.sync.get("rumbleChatSrc", function(src) {
-                            src = src.rumbleChatSrc
-                            rumbleChat = document.getElementsByClassName("chat--container")[0]
-                            while (rumbleChat.firstChild) {
-                                rumbleChat.removeChild(rumbleChat.firstChild);
-                            }
-                            rumbleChat.setAttribute("style", "flex-direction: column; -webkit-flex-direction: column;")
-                            chat = document.createElement("iframe")
-                            chat.setAttribute("id", "dggChat")
-                            chat.setAttribute("class", "chat--container container")
-                            chat.setAttribute("style", "width: 400px; border: 0;")
-                            chat.setAttribute("src", src)     
-                            rumbleChat.appendChild(chat)
-                        });
-                    }
-                }
-            });
-        });    
-    }
-});
-
-$('body').on('click', 'a[target="popout"]', function(x) {
-    x.preventDefault();
-    chrome.tabs.create({ url: $(this).prop('href'), active: false });
-    return false;
-});
+				chrome.scripting.executeScript({
+					target: { tabId: tab.id },
+					function: async () => {
+						if (window.location.href.indexOf("youtube.com") > -1) {
+							chrome.storage.sync.get("youtubeChatSrc", function (result) {
+								var currentChat = document.getElementsByTagName(
+									"ytd-live-chat-frame"
+								)[0]
+								while (currentChat.firstChild) {
+									currentChat.removeChild(currentChat.firstChild)
+								}
+								currentChat.setAttribute(
+									"style",
+									"flex-direction: column; -webkit-flex-direction: column;"
+								)
+								var newChat = document.createElement("iframe")
+								newChat.setAttribute("id", "chatframe")
+								newChat.setAttribute("class", "style-scope ytd-live-chat-frame")
+								newChat.setAttribute("frameborder", "0")
+								newChat.setAttribute("scrolling", "no")
+								newChat.setAttribute("src", result.youtubeChatSrc)
+								currentChat.appendChild(newChat)
+							})
+						}
+						if (window.location.href.indexOf("twitch.tv") > -1) {
+							chrome.storage.sync.get("twitchChatSrc", function (src) {
+								var currentChat = document.getElementsByClassName(
+									"channel-root__right-column"
+								)[0]
+								while (currentChat.firstChild) {
+									currentChat.removeChild(currentChat.firstChild)
+								}
+								var newChat = document.createElement("iframe")
+								newChat.setAttribute("id", "dggChat")
+								newChat.setAttribute("class", "chatframe")
+								newChat.setAttribute(
+									"style",
+									"height: 100% !important; width: 100% !important; display: block;"
+								)
+								newChat.setAttribute("src", src.twitchChatSrc)
+								currentChat.prepend(newChat)
+							})
+						} else if (window.location.href.indexOf("kick.com") > -1) {
+							chrome.storage.sync.get("kickChatSrc", function (src) {
+								var currentChat = document.getElementsByClassName("chatroom")[0]
+								while (currentChat.firstChild) {
+									currentChat.removeChild(currentChat.firstChild)
+								}
+								currentChat.setAttribute(
+									"style",
+									"flex-direction: row -webkit-flex-direction: row"
+								)
+								var newChat = document.createElement("iframe")
+								newChat.setAttribute("id", "dggChat")
+								newChat.setAttribute("class", "chatframe")
+								newChat.setAttribute("style", "flex: auto")
+								newChat.setAttribute("src", src.kickChatSrc)
+								currentChat.appendChild(newChat)
+							})
+						} else if (window.location.href.indexOf("rumble.com") > -1) {
+							chrome.storage.sync.get("rumbleChatSrc", function (src) {
+								var currentChat =
+									document.getElementsByClassName("chat--container")[0]
+								console.log(
+									"%c[DGG] %cPEEPO POOFING RUMBLE CHAT AWAY",
+									"color: #538CC6",
+									"color: #6F859A"
+								)
+								// remove all children from the chat
+								while (currentChat.firstChild) {
+									currentChat.removeChild(currentChat.firstChild)
+								}
+								// add the iframe with dgg chat
+								var newChat = document.createElement("iframe")
+								newChat.setAttribute("id", "dggChat")
+								newChat.setAttribute("class", "chat--container container")
+								newChat.setAttribute(
+									"style",
+									"height: 660px; width: 400px; border: 0;"
+								)
+								newChat.setAttribute("src", src.rumbleChatSrc)
+								currentChat.appendChild(newChat)
+							})
+						}
+					},
+				})
+			}
+		)
+	}
+})
