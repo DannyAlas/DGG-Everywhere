9c03ab9b610fcc061d8fa024cb91fdacabb09b22
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2023-03-25 21:09:34 -0600

    cleanup
    moved from jQuery to JS native, added linting, basic styling

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		webextensions: true,
+	},
+	extends: "eslint:recommended",
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+	},
+	rules: {
+		indent: ["error", "tab"],
+		semi: ["error", "never"],
+	},
+}
