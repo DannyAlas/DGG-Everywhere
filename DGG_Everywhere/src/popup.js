ad03cfc093c3128478e843d8f8a168cd52074204
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2023-03-30 22:28:38 -0600

    Options and permissions (#11)
    * Options page
    * Better Permissions
    * Bootstrap

--- DGG_Everywhere/src/popup.js
+++ DGG_Everywhere/src/popup.js
@@ -1,157 +1,163 @@
 const checkbox = document.querySelector("input[name=checkbox]")
-chrome.storage.sync.get(["checkbox"], async function (result) {
-	if (result.checkbox != null) {
-		document.getElementById("checkbox").checked = result.checkbox
-	} else {
-		document.getElementById("checkbox").checked = false
+
+function getOrigin(url) {
+	// return the origin of the url or null if it is not a possible origin
+	let possibleOrigins = [
+		"*://*.youtube.com/*",
+		"*://*.twitch.tv/*",
+		"*://*.destiny.gg/*",
+		"*://*.kick.com/*",
+		"*://*.rumble.com/*",
+	]
+	for (let origin of possibleOrigins) {
+		const regex = /\.([^/]+)\./
+		if (url.match(regex.exec(origin)[1])) {
+			return origin
+		}
 	}
-})
+	return null
+}
+
+function getDomainFromOrigin(url) {
+	// "*://*.kick.com/*" -> "kick.com"
+	if (url == null) {
+		return null
+	}
+	let regex = /(\*:\/\/\*\.)|(\*\/\*)|(\*)|(\/)/g
+	return url.replace(regex, "")
+}
+
+function getDomainFromUrl(url) {
+	let hostname = new URL(url).hostname
+	if (hostname.indexOf("www.") > -1) {
+		hostname = hostname.substring(hostname.indexOf(".") + 1)
+	}
+	return hostname
+}
+
+async function getSelectedOrigins() {
+	let origins = []
+	await chrome.permissions.getAll().then((permissions) => {
+		for (let origin of permissions.origins) {
+			origins.push(origin)
+		}
+	})
+	return origins
+}
+
+async function registerContentScript() {
+	chrome.scripting.getRegisteredContentScripts().then(async (scripts) => {
+		if (!scripts.find((script) => script.id === "chat-injector")) {
+			let origins = await getSelectedOrigins()
+			chrome.scripting.registerContentScripts([
+				{
+					id: "chat-injector",
+					matches: origins,
+					js: ["src/content.js"],
+					runAt: "document_start",
+				},
+			])
+		}
+	})
+}
+
+async function doWeHavePermission(origin) {
+	let perms = await chrome.permissions.getAll()
+	return perms.origins.includes(origin)
+}
+
+async function setCheckbox() {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+	let origin = getOrigin(tab.url)
+	chrome.storage.sync.get(
+		[`checkbox-${getDomainFromOrigin(origin)}`],
+		async function (result) {
+			if (result[`checkbox-${getDomainFromOrigin(origin)}`] != null) {
+				await doWeHavePermission(origin).then((perms) => {
+					if (perms) {
+						document.getElementById("checkbox").checked =
+							result[`checkbox-${getDomainFromOrigin(origin)}`]
+					} else {
+						chrome.storage.sync.set({
+							[`checkbox-${getDomainFromOrigin(origin)}`]: false,
+						})
+						document.getElementById("checkbox").checked = false
+					}
+				})
+			} else {
+				document.getElementById("checkbox").checked = false
+			}
+		}
+	)
+}
+
+async function init() {
+	await registerContentScript()
+	await setCheckbox()
+}
+
+init()
+
 checkbox.addEventListener("change", async (e) => {
 	if (e.target.checked) {
-		chrome.storage.sync.set({ checkbox: true })
+		// ensure the content script is registered
 		let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-
-		chrome.scripting.executeScript({
-			target: { tabId: tab.id },
-			function: async () => {
-				var DGGurl = "https://www.destiny.gg/embed/chat"
-				if (window.location.href.indexOf("youtube.com/watch") > -1) {
-					// save youtube url to storage for the popup
-					if (
-						document.getElementById("chatframe").getAttribute("src") !== null
-					) {
-						chrome.storage.sync.set({
-							youtubeChatSrc: document
-								.getElementById("chatframe")
-								.getAttribute("src"),
-						})
-					} else {
-						// save youtube url to storage for the popup
-						chrome.storage.sync.set({
-							youtubeChatSrc:
-								"https://www.youtube.com/live_chat?v=" +
-								window.location.href.substring(
-									window.location.href.lastIndexOf("=") + 1
-								),
-						})
-					}
-					// load the dgg chat
-					var currentChat = document.getElementsByTagName(
-						"ytd-live-chat-frame"
-					)[0]
-					console.log(
-						"%c[DGG] %cPEEPO POOFING YOUTUBE CHAT AWAY",
-						"color: #538CC6",
-						"color: #6F859A"
-					)
-					while (currentChat.firstChild) {
-						currentChat.removeChild(currentChat.firstChild)
-					}
-					currentChat.setAttribute(
-						"style",
-						"flex-direction: row -webkit-flex-direction: row"
-					)
-					var newChat = document.createElement("iframe")
-					newChat.setAttribute("id", "dggChat")
-					newChat.setAttribute("class", "chatframe")
-					newChat.setAttribute("style", "flex: auto")
-					newChat.setAttribute("src", DGGurl)
-					currentChat.appendChild(newChat)
-				}
-				if (window.location.href.indexOf("twitch.tv") > -1) {
-					// set the twitch chat src from the window url
-					chrome.storage.sync.set({
-						twitchChatSrc: `https://www.twitch.tv/popout/${window.location.href.substring(
-							window.location.href.lastIndexOf("/") + 1
-						)}/chat?popout=`,
-					})
-					let currentChat = document.getElementsByClassName(
-						"channel-root__right-column"
-					)[0]
-					// get the chatframe and empty it
-					if (currentChat) {
-						console.log(
-							"%c[DGG] %cPEEPO POOFING TWITCH CHAT AWAY",
-							"color: #538CC6",
-							"color: #6F859A"
-						)
-						while (currentChat.firstChild) {
-							currentChat.removeChild(currentChat.firstChild)
+		let origin = getOrigin(tab.url)
+		if (origin == null) {
+			document.getElementById("checkbox").checked = false
+			return
+		}
+		await doWeHavePermission(origin).then(async (perms) => {
+			if (!perms) {
+				await chrome.permissions
+					.request({ origins: [origin] })
+					.then(async (granted) => {
+						if (granted) {
+							// update the content script to the new permissions
+							let origins = await getSelectedOrigins()
+							await chrome.scripting.updateContentScripts([
+								{
+									id: "chat-injector",
+									matches: origins,
+								},
+							])
+							chrome.storage.sync.set({
+								[`checkbox-${getDomainFromUrl(tab.url)}`]: true,
+							})
+							chrome.scripting.executeScript({
+								target: { tabId: tab.id },
+								files: ["src/content.js"],
+							})
 						}
-						let newChat = document.createElement("iframe")
-						newChat.setAttribute("id", "dggChat")
-						newChat.setAttribute("class", "chatframe")
-						newChat.setAttribute(
-							"style",
-							"height: 100% !important; width: 100% !important; display: block;"
-						)
-						newChat.setAttribute("src", DGGurl)
-						currentChat.prepend(newChat)
-					}
-				}
-				if (window.location.href.indexOf("kick.com") > -1) {
-					// save kicks popup chat url to storage
-					chrome.storage.sync.set({
-						kickChatSrc: `https://kick.com/${window.location.href.substring(
-							window.location.href.lastIndexOf("/") + 1
-						)}/chatroom`,
 					})
-					let currentChat = document.getElementsByClassName("chatroom")[0]
-					console.log(
-						"%c[DGG] %cPEEPO POOFING KICK CHAT AWAY",
-						"color: #538CC6",
-						"color: #6F859A"
-					)
-					// remove all children from the chat
-					while (currentChat.firstChild) {
-						currentChat.removeChild(currentChat.firstChild)
-					}
-					// set the flex direction to row
-					currentChat.setAttribute(
-						"style",
-						"flex-direction: row -webkit-flex-direction: row"
-					)
-					// add the iframe with dgg chat
-					let newChat = document.createElement("iframe")
-					newChat.setAttribute("id", "dggChat")
-					newChat.setAttribute("class", "chatframe")
-					newChat.setAttribute("style", "flex: auto")
-					newChat.setAttribute("src", "https://www.destiny.gg/embed/chat")
-					currentChat.appendChild(newChat)
-				}
-				if (window.location.href.indexOf("rumble.com") > -1) {
-					chrome.storage.sync.set({
-						rumbleChatSrc: `https://rumble.com/chat/popup/${document
-							.getElementsByClassName("rumbles-vote")[0]
-							.getAttribute("data-id")}`,
-					})
-					let currentChat =
-						document.getElementsByClassName("chat--container")[0]
-					console.log(
-						"%c[DGG] %cPEEPO POOFING RUMBLE CHAT AWAY",
-						"color: #538CC6",
-						"color: #6F859A"
-					)
-					// remove all children from the chat
-					while (currentChat.firstChild) {
-						currentChat.removeChild(currentChat.firstChild)
-					}
-					// add the iframe with dgg chat
-					let newChat = document.createElement("iframe")
-					newChat.setAttribute("id", "dggChat")
-					newChat.setAttribute("class", "chat--container container")
-					newChat.setAttribute(
-						"style",
-						"height: 660px; width: 400px; border: 0;"
-					)
-					newChat.setAttribute("src", "https://www.destiny.gg/embed/chat")
-					currentChat.appendChild(newChat)
-				}
-			},
+			} else if (perms) {
+				let origins = await getSelectedOrigins()
+				// update the content script to the new permissions
+				await chrome.scripting.updateContentScripts([
+					{
+						id: "chat-injector",
+						matches: origins,
+					},
+				])
+				chrome.storage.sync.set({
+					[`checkbox-${getDomainFromUrl(tab.url)}`]: true,
+				})
+				chrome.scripting.executeScript({
+					target: { tabId: tab.id },
+					files: ["src/content.js"],
+				})
+			}
 		})
 	} else if (!e.target.checked) {
-		chrome.storage.sync.set({ checkbox: false })
-
+		let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+		let origin = getOrigin(tab.url)
+		if (origin == null) {
+			document.getElementById("checkbox").checked = false
+			return
+		}
+		chrome.storage.sync.set({
+			[`checkbox-${getDomainFromUrl(tab.url)}`]: false,
+		})
 		chrome.tabs.query(
 			{
 				active: true,
@@ -163,7 +169,12 @@ checkbox.addEventListener("change", async (e) => {
 				chrome.scripting.executeScript({
 					target: { tabId: tab.id },
 					function: async () => {
-						if (window.location.href.indexOf("youtube.com") > -1) {
+						console.log(
+							"%c[DGG] %cPEEPO POOFING DGG CHAT AWAY",
+							"color: #538CC6",
+							"color: #6F859A"
+						)
+						if (window.location.href.indexOf("youtube.com/watch") > -1) {
 							chrome.storage.sync.get("youtubeChatSrc", function (result) {
 								var currentChat = document.getElementsByTagName(
 									"ytd-live-chat-frame"
@@ -254,7 +265,7 @@ checkbox.addEventListener("change", async (e) => {
 // you gotta have some fun... right? (also im too artistically inept to make this extenion look nice)
 chrome.runtime.sendMessage(
 	{
-		contentScriptQuery: "getEmotes",
+		backgroundScriptQuery: "getEmotes",
 		url: "https://cdn.destiny.gg/emotes/emotes.json",
 	},
 	function (response) {
